cf38eeb9c48646e1f77cd165b34e0c3cd5976f73
Nathan M <nathanxma@users.noreply.github.com>
2015-01-31 20:12:15 -0600

    Update __init__.py

--- GreenMoon/__init__.py
+++ GreenMoon/__init__.py
@@ -9,7 +9,16 @@ app = Flask(__name__)
 ## SQL DB for core/structured storage, such as  by organizing or parsing data from MongoDB
 # Set up connection with postgresql
 app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
+dbSQL = SQLAlchemy(app)
+
+## Initialize dbSQL and and admin
+dbSQL.drop_all()
+dbSQL.create_all()
+admin = Account(name='admin', \
+                password_hash=generate_password_hash('admin'))
+dbSQL.session.add(admin)
+dbSQL.session.commit()
+
 
 ## MongoDB for fast unstructured data storage, such as info crawled from webpages, twitter, etc.
 # Set up mongoDB engine
