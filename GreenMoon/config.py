6520ec2f0d52cd194190a2c8781016bec72a3db6
bearicc <hxiongustc@gmail.com>
2015-01-31 19:26:04 -0600

    Add comments on how to create the default account on the database server

--- GreenMoon/config.py
+++ GreenMoon/config.py
@@ -1,6 +1,8 @@
 import os
 
-# database url
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bear:123@localhost/blogdb'
+# Use the following commands to create an account on the database server:
+#   CREATE USER 'greenmoon'@'localhost' IDENTIFIED BY 'greenmoon';
+#   GRANT ALL PRIVILEGES ON *.* TO 'greenmoon'@'localhost' WITH GRANT OPTION;
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://greenmoon:greenmoon@localhost/bbsdb'
 # True: Enable automatic commits of database changes at the end of each request.
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
