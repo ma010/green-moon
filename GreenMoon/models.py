c381b20d3e318e7ba60bf804477a2089cbb8a01c
Nathan M <nathanxma@users.noreply.github.com>
2015-01-31 20:10:45 -0600

    Update models.py

--- GreenMoon/models.py
+++ GreenMoon/models.py
@@ -1,5 +1,28 @@
-from GreenMoon import app, db, dbMongo
+from GreenMoon import app, dbSQL, dbMongo
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
+class Account(dbSQL.Model):
+    __tablename__ = 'accounts'
+    name = dbSQL.Column(dbSQL.String(12), primary_key=True, unique=True)
+    password_hash = dbSQL.Column(dbSQL.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password cannot be read directly!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<Account name %r>' % self.name
+
+# block below is temporarily abandoned
+'''
 # define a User table to store user id, nickname, email and posts
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,13 +33,15 @@ class Account(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+'''
 
 
-# define a class Post -- a second database
+# define a class Post -- a second table in the database
 # to store User's Post separately
 
 
-# mongdb where business license info is stored
+## Needs more input from Bo
+# Define a method to draw data from MongoDB and then print
 def allTupleFromDB():
     output = ""
     allLicense = dbMongo.activeLicense.find()
