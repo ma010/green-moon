b0f52d1c04f14fa516c47de967a01cbc8816ff28
Nathan M <nathanxma@users.noreply.github.com>
2015-01-31 20:03:57 -0600

    Update __init__.py

--- GreenMoon/__init__.py
+++ GreenMoon/__init__.py
@@ -1,17 +1,21 @@
-import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from .config import basedir
 from pymongo import Connection
 
 app = Flask(__name__)
 
-# set up db connection with mongoDB
-con = Connection()
-db = con.businessDB
 
-# set up dbSQL connection with postgresql
+## SQL DB for core/structured storage, such as  by organizing or parsing data from MongoDB
+# Set up connection with postgresql
 app.config.from_pyfile('config.py')
-dbSQL = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-from GreenMoon import views
+## MongoDB for fast unstructured data storage, such as info crawled from webpages, twitter, etc.
+# Set up mongoDB engine
+con = Connection()
+# Get database record - businessDB which contains business lincense info
+dbMongo = con.businessDB
+
+
+from GreenMoon import models, views
