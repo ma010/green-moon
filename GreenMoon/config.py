dab66fe82e1ff9c3683776b7e0933c7121c77bfc
bearicc <hxiongustc@gmail.com>
2015-01-31 19:10:54 -0600

    Do not check optional url at present.

--- GreenMoon/config.py
+++ GreenMoon/config.py
@@ -1,6 +1,5 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 # database url
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bear:123@localhost/blogdb'
 # True: Enable automatic commits of database changes at the end of each request.
