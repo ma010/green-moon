7f8f441de3775d1db584764164a5bbaa7f05316d
Nathan M <nathanxma@users.noreply.github.com>
2015-01-31 19:52:01 -0600

    Update config.py

--- GreenMoon/config.py
+++ GreenMoon/config.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-SECRET_KEY = 'guess'
-
+# This will make adjustment of URI more flexible in the future
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = ('postgresql+psycopg2://nathan:789@localhost/blog.db')
+    SQLALCHEMY_DATABASE_URI = ('postgresql+psycopg2://greenmoon:greenmoon@localhost/blog.db')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
+# a repository for database migration 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_RECORD_QUERIES = True
+
+# Unknown feature; check back later
+#SQLALCHEMY_RECORD_QUERIES = True
 
 # administrator list
-ADMINS = ['greenoony@gmail.com']
+
