f8d77997a537803cbf4067a21013fc28ef0cc78c
Hao Xiong <bear@debian>
2015-02-06 17:17:45 -0600

    move db_init out of app

--- GreenMoon/__init__.py
+++ GreenMoon/__init__.py
@@ -1,10 +1,12 @@
-
 from flask import Flask
-#from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 from pymongo import Connection
 #from werkzeug.security import generate_password_hash, check_password_hash
-
+#from werkzeug.security import generate_password_hash
+## SQL DB for core/structured storage, such as  by organizing or parsing data from MongoDB
+# Set up connection with postgresql
 app = Flask(__name__)
+dbSQL = SQLAlchemy(app)
 
 
 ## SQL DB for core/structured storage, such as  by organizing or parsing data from MongoDB
