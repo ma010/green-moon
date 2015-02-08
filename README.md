fccf835a72cdd881c89fb355cfc442e84f9ba0b3
bearicc <hxiongustc@gmail.com>
2015-02-07 22:08:29 -0600

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+# green-moon
+Run db_init.py first to init the database.
+
+Then run.py to start a server.
---
+++ run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from GreenMoon import app as application
+# ----------
+# The above code can be put into a file called GreenMoon.wsgi for Apache deployment
+application.run(debug=True)
+
