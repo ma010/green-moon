a48e8492a68213a0251755ab8b3230673f29419a
ma <cloud.ma@icloud.com>
2018-02-19 21:19:46 -0500

    updated forms.py

--- GreenMoon/forms.py
+++ GreenMoon/forms.py
@@ -1,7 +1,11 @@
+"""
+Implement a class function for user to put in a zip-code and search relevant information about business entities
+in that zip-code area.
+"""
+
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
-
 
 class inputZipForm(Form):
     inputZip = StringField('inputZip', validators=[DataRequired()])
