6efb2e984616a0f3d94b528de281a79b383b5e5a
ma <cloud.ma@icloud.com>
2018-02-19 20:59:38 -0500

    updated config.py

--- GreenMoon/config.py
+++ GreenMoon/config.py
@@ -1,20 +1,20 @@
+'''
+This script configures the database
+'''
+
 # -*- coding: utf-8 -*-
 import os
 
-# This will make adjustment of URI more flexible in the future
+# This will make adjustment of database URL more flexible in the future
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = ('postgresql+psycopg2://greenmoon:greenmoon@localhost/blogdb')
+    SQLALCHEMY_DATABASE_URI = ('postgresql+psycopg2://PATH:PASSWORD@localhost/DATABASENAME')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-# True: Enable automatic commits of database changes at the end of each request.
+# True: Enable automatic commits of database changes at the end of each request
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     
 # a repository for database migration 
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-# Unknown feature; check back later
-#SQLALCHEMY_RECORD_QUERIES = True
-
-# administrator list
 
